8298e0f2b241196dc94d7447ece4b9a53d8d3dee
Jean Pierre Vera <jpierre22391@hotmail.com>
2019-11-29 20:18:44 -0500

    Added empty initial components and folders

---
+++ src/ColorSyncApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class ColorSyncApp extends Component {
+  render (){
+    return (
+      <div className="App">
+        something here
+      </div>
+    );
+  }
+}
+
+export default ColorSyncApp;
